56eeb282516b1a3c456446a3abf9e8add7dbf29b
onsubat <m.b.altintas@hotmail.com>
2022-01-09 03:43:57 +0300

    patika_csharp_degiskenler

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//datetime picker
+DateTime tarih = DateTime.Now;
+Console.WriteLine(tarih);
+
+//date picker
+String gunn = DateTime.Now.ToString("dd.MM.yyyy");
+Console.WriteLine(gunn);
+
+//time picker
+String saat1 = DateTime.Now.ToString("HH:mm");
+Console.WriteLine(saat1);
+
